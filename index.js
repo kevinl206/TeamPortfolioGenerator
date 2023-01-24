6e6cab04542dd21cb8d1fc69f06c5bdeea3efe0d
Kevin Lang <kevinl20@yahoo.com>
2023-01-23 18:06:01 -0800

    Added function for engineer in the index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const Employee = require("./lib/Employee");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 
+// Manager
 function addManager() {
     inquirer
        .prompt([
@@ -34,3 +35,29 @@ function addManager() {
                 message: "What is the manager's office number?"
             }
        ])};
+
+//Engineer
+function addEngineer() {
+    inquirer
+        .prompt([
+             {
+                 type: "input",
+                 name: "name",
+                 message: "What is the engineer's name?"
+                },
+                {
+                 type: "input",
+                 name: "id",
+                 message: "What is the engineer's ID?"
+                },
+                {
+                 type: "input",
+                 name: "email",
+                 message: "What is the engineer's email?"
+                },
+                {
+                 type: "input",
+                 name: "github",
+                 message: "What is the engineer's GitHub username?"
+                }
+        ])};
