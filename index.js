ede23aea8c038ebcb1adeb0ed32b0b979c0e075a
Kevin Lang <kevinl20@yahoo.com>
2023-01-23 18:10:07 -0800

    Added intern function in index.js

--- index.js
+++ index.js
@@ -61,3 +61,29 @@ function addEngineer() {
                  message: "What is the engineer's GitHub username?"
                 }
         ])};
+
+//Intern
+function addIntern() {
+    inquirer
+       .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the intern's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the intern's ID?"
+                },
+                {
+                type: "input",
+                name: "email",
+                message: "What is the intern's email?"
+                },
+                {
+                type: "input",
+                name: "school",
+                message: "What school does the intern attend?"
+                }
+        ])};
