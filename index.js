cf094f550c298bf52c02a831cb0c5f6774b42da3
Kevin Lang <kevinl20@yahoo.com>
2023-05-05 22:34:03 -0700

    added test folder and code to employee

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
-
+const jest = require('jest');
 //constructor
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Employee = require("./lib/Employee");
 
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "index.html");
+const DIST_DIR = path.resolve(__dirname, 'dist')
+const outputPath = path.join(DIST_DIR, 'index.html');
 
-const render = require('./util/generatehtml');
+const render = require('./util/generateHtml.js');
 
 // Team members array
 const teamMembers = [];
